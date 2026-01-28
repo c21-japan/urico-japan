185c83142de227f2f6e04a482ae2d753e86df97a
c21-japan <y-inui@century21.group>
2026-01-28 09:55:52 +0900

    Fix: Remove part file dependencies and add voice images

--- house_db.js
+++ house_db.js
@@ -1,17 +1,7 @@
 // 戸建データベース
 // AUTO-GENERATED. DO NOT EDIT BY HAND.
-// 完成しているJSONファイルから自動生成
-// 分割: data/house/part1.js ～ part3.js（scripts/generate_house_db.mjs で生成）
-// 全6121件の購入希望者情報
+// 実際のデータはCloudflare R2に保存されており、検索時にR2にリダイレクトします
 
-import { HOUSE_DB_PART as PART1 } from './data/house/part1.js';
-import { HOUSE_DB_PART as PART2 } from './data/house/part2.js';
-import { HOUSE_DB_PART as PART3 } from './data/house/part3.js';
-
-export const HOUSE_DB = [
-  ...PART1,
-  ...PART2,
-  ...PART3
-];
+export const HOUSE_DB = [];
 
 export default HOUSE_DB;
--- land_db.js
+++ land_db.js
@@ -1,13 +1,7 @@
-// 土地購入希望者データベース
+// 土地データベース
 // AUTO-GENERATED. DO NOT EDIT BY HAND.
-// 全6229件の購入希望者情報
+// 実際のデータはCloudflare R2に保存されており、検索時にR2にリダイレクトします
 
-import { LAND_DB_PART1 } from './data/land/part1.js';
-import { LAND_DB_PART2 } from './data/land/part2.js';
-import { LAND_DB_PART3 } from './data/land/part3.js';
+export const LAND_DB = [];
 
-export const LAND_DB = [
-  ...LAND_DB_PART1,
-  ...LAND_DB_PART2,
-  ...LAND_DB_PART3
-];
+export default LAND_DB;
